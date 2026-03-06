f161f21838018d5454c86740b14ce83a399e4107
gabriel <gabrielbunselmeyer@gmail.com>
2026-03-06 21:07:36 +0100

    DebugLogging as a setting

--- ErasPortraitSelector/Mod.cs
+++ ErasPortraitSelector/Mod.cs
@@ -2,18 +2,34 @@ using System;
 using System.IO;
 using System.Reflection;
 using HarmonyLib;
+using Newtonsoft.Json;
 
 namespace ErasPortraitSelector
 {
+    public class ModSettings
+    {
+        public bool DebugLogging = false;
+    }
+
     public static class Mod
     {
         internal static string ModDirectory;
         private static string LogPath;
 
-        internal static readonly bool ShouldLog = false;
+        public static ModSettings Settings = new();
+        internal static bool ShouldLog => Settings.DebugLogging;
 
         public static void Init(string directory, string settingsJSON)
         {
+            try
+            {
+                if (!string.IsNullOrEmpty(settingsJSON))
+                {
+                    Settings = JsonConvert.DeserializeObject<ModSettings>(settingsJSON) ?? new ModSettings();
+                }
+            }
+            catch (Exception) { }
+
             ModDirectory = directory;
             LogPath = Path.Combine(directory, "ErasPortraitSelector.log");
             File.WriteAllText(LogPath, "");
