004a96feca406bff71eb732652528e8fbff37231
gabriel <gabrielbunselmeyer@gmail.com>
2026-03-06 20:33:34 +0100

    Initial mod structure

---
+++ ErasPortraitSelector/Mod.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Reflection;
+using HarmonyLib;
+
+namespace ErasPortraitSelector
+{
+    public static class Mod
+    {
+        internal static string ModDirectory;
+        private static string LogPath;
+
+        internal static readonly bool IsDebug = false;
+
+        public static void Init(string directory, string settingsJSON)
+        {
+            ModDirectory = directory;
+            LogPath = Path.Combine(directory, "ErasPortraitSelector.log");
+            File.WriteAllText(LogPath, "");
+
+            var harmony = new Harmony("era.ErasPortraitSelector");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log("Initialized");
+        }
+
+        public static void Log(string message)
+        {
+            if (!IsDebug)
+            {
+                return;
+            }
+
+            try
+            {
+                using var writer = new StreamWriter(LogPath, true);
+                writer.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+            }
+            catch { }
+        }
+    }
+}
